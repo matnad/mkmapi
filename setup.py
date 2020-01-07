8bb94b59617985e2a3955c9ce68293b1d382eb1f
Matthias Nadler <matnad@gmail.com>
2020-01-07 16:22:21 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mkmapi',
+    version='1.0.0',
+    packages=['mkmapi', 'mkmapi.api_map'],
+    url='https://github.com/matnad/mkmapi',
+    license='MIT',
+    author='Matthias Nadler',
+    author_email='matnad@gmail.com',
+    description='A full wrapper for the MKM API 2.0'
+)
